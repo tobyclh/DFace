200eb103a997d725b078b7293b5a37297e3315ad
Toby the potato <tobyclh@gmail.com>
2019-03-04 18:12:43 -0800

    add dface setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages, distutils
+
+
+setup(name='DFace',
+      version='0.1',
+      description='',
+      url='http://github.com/tobyclh/DFace',
+      author='tobyclh',
+      author_email='tobyclh@embodyme.com',
+      license='Copyright',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=False)
